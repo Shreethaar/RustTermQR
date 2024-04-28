55b10ea2d242827ece3c98cff8ee5eed1fd9e2ce
Shreethaar <shreethaara380@gmail.com>
2024-04-29 00:35:36 +0800

    Trying out rqrr v0.7.1

--- QRterm-rust/src/main.rs
+++ QRterm-rust/src/main.rs
@@ -1,6 +1,12 @@
-mod scanner;
+use image;
+use rqrr;
 
-fn main() {
-    scanner::scan_qr_code();
-}
+static img = image::open("tests/data/github.gif")?.to_luma();
+let mut img = rqrr::PreparedImage::prepare(img);
+let grids = img.detect_grids();
+assert_eq!(grids.len(), 1);
+
+let (meta, content) = grids[0].decode()?;
+assert_eq!(meta.ecc_level, 0);
+assert_eq!(content, "https://github.com/WanzenBug/rqrr");
 
